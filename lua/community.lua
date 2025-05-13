05aaf221e65656782b39d41429028b8fb769911a
ev.shevchenko <ev.shevchenko@vk.team>
2025-05-13 15:15:59 +0300

    add neo0test plugin and python templater

--- lua/community.lua
+++ lua/community.lua
@@ -9,8 +9,14 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.scala" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.markdown-and-latex.vimtex" },
-  -- { import = "astrocommunity.note-taking.obsidian-nvim" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
   { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.editing-support.dial-nvim" },
+  { import = "astrocommunity.editing-support.neogen" },
+  { import = "astrocommunity.media.image-nvim" },
+  { import = "astrocommunity.code-runner.vim-slime" },
   -- import/override with your plugins folder
 }
---
+++ templates/python/arparseapp.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+def main():
+    
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--{{_cursor_}}")
+    main(**vars(parser.parse_args()))
+    
